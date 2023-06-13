586e5226f09b9ff88db88c9f5361c2870c089827
manou141 <manou141@hotmail.com>
2023-06-13 00:51:30 -0400

    added the dashboard to the navbar

--- shop-app/resources/views/product.blade.php
+++ shop-app/resources/views/product.blade.php
@@ -95,21 +95,24 @@
                                     <a data-scroll-nav="0" href="#contact">Contact</a>
                                 </li>
                                 @if (Route::has('login'))
-                                <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
-                                    @auth
-                                    <a href="{{ url('/home') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Home</a>
-                                    @else
+                                @auth
+                                <li class="nav-item">
+                                    <a href="{{ url('/home') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                                </li>
+                                @else
+                                <li class="nav-item">
                                     <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
-
-                                    @if (Route::has('register'))
-                                    <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
-                                    @endif
-                                    @endauth
-                                </div>
+                                </li>
+                                @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a href="{{ route('register') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                                </li>
                                 @endif
-
+                                @endauth
+                                @endif
                             </ul> <!-- navbar nav -->
                         </div>
+
                     </nav> <!-- navbar -->
                 </div>
             </div> <!-- row -->
